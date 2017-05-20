fe43e2b32fc66976874a923a8109f21cc8cd035d
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-19 20:47:07 -0400

    finished lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :anagram
+  def initialize anagram
+    @anagram = anagram
+    @anagram_chars = @anagram.split("").sort
+  end
+  def match match_ary #array of words
+    match_ary.map do |word|
+      term_chars = word.split("").sort
+      if term_chars == @anagram_chars
+        word
+      end
+    end.compact
+  end
+end
